2180afdf585fe7410a7759452df6abdb6a44ec48
Piyush Singh <singh.piyush00034@gmail.com>
2024-03-28 16:44:38 +0530

    Added circuitBreaker

--- src/main/java/com/microservice/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/microservice/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.microservice.currencyexchangeservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +13,8 @@ import org.springframework.web.client.RestTemplate;
 public class CircuitBreakerController {
     private Logger logger= LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api",fallbackMethod = "hardcodedResponse")
+//    @Retry(name = "sample-api",fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "sample-api",fallbackMethod = "hardcodedResponse")
     public String SampleAPI(){
         logger.info("Sample API call received");
         ResponseEntity<String> responseEntity=new  RestTemplate().getForEntity("http://localhost:8080/some-dummy",String.class);
