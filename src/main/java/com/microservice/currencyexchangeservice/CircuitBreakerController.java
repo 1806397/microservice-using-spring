30f271a93669a27411f41a0ed4d9fd857580cdf1
Piyush Singh <singh.piyush00034@gmail.com>
2024-03-28 16:59:48 +0530

    BulkHead

--- src/main/java/com/microservice/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/microservice/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.microservice.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -16,7 +17,8 @@ public class CircuitBreakerController {
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api",fallbackMethod = "hardcodedResponse")
 //    @CircuitBreaker(name = "sample-api",fallbackMethod = "hardcodedResponse")
-    @RateLimiter(name = "default")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "sample-api")
     public String SampleAPI(){
         logger.info("Sample API call received");
         ResponseEntity<String> responseEntity=new  RestTemplate().getForEntity("http://localhost:8080/some-dummy",String.class);
