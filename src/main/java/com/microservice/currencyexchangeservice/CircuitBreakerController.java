4d7207c7123258e2fb19a2c737bc46ca98bd71fa
Piyush Singh <singh.piyush00034@gmail.com>
2024-03-28 16:34:23 +0530

    Retry in Circuit Breaker

--- src/main/java/com/microservice/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/microservice/currencyexchangeservice/CircuitBreakerController.java
@@ -1,12 +1,24 @@
 package com.microservice.currencyexchangeservice;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
+    private Logger logger= LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
+    @Retry(name = "sample-api",fallbackMethod = "hardcodedResponse")
     public String SampleAPI(){
-        return "Sample API";
+        logger.info("Sample API call received");
+        ResponseEntity<String> responseEntity=new  RestTemplate().getForEntity("http://localhost:8080/some-dummy",String.class);
+        return responseEntity.getBody();
+    }
+    public String hardCodedResponse(Exception ex){
+        return "fallbackResponse";
     }
 }
